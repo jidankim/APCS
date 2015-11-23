3358d60af872b27bfa9dfad118b8fb65d92b4ed3
Dan Kim <heirapparent1126@hotmail.com>
2015-11-23 06:52:26 -0500

    l

--- NumToString.java
+++ NumToString.java
@@ -10,20 +10,23 @@ public class NumToString{
     
     public static String twoDigitToEng(int n) {
 	String[] teen = new String[10];
-	twoDigit[0] = "ten";
-	twoDigit[1] = "eleven";
-	twoDigit[2] = "twelve";
-	twoDigit[3] = "thirteen";
+	teen[0] = "ten";
+	teen[1] = "eleven";
+	teen[2] = "twelve";
+	teen[3] = "thirteen";
 	for (int i = 4; i < 10; i++) {
-	    twoDigit[i] = digitToEng(i) + "teen";
+	    teen[i] = digitToEng(i) + "teen";
 	}
-	String[] twoDigit = {"twenty". "thirty", "fourty";
+	String[] twoDigit = {"twenty". "thirty", "fourty", "fifty", "sixty", "seventy", "eighty", "ninety"};
+
+	if (n/10 == 1) return teen[n%10];
+	else return twoDigit[n/10 - 2] + digitToEng[n%10];
     }
     
     // pre-condition: 0 <= n < 1,000,000
     // post-condition: 0 -> "zero", ..., 25 -> "twenty-five", ..., 125 -> "one hundred twenty-five"
     public static String numToEng(int n) {
-	String[] suffix = {"million", "thousand", "hundred"};
+	String[] suffix = {"thousand", "hundred"};
 	if (n < 10) return digitToEng(n);
 	else if (n < 100) return twoDigitToEng(n);
 	else return digitToEng(Integer.parseInt(Integer.toString(n).substring(0, 1))) + numToEng(Integer.parseInt(Integer.toString(n).substring(1)));
