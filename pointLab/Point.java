266efa34fea27c2a53074f48727c05006ada8023
Dan Kim <jidankim@gmail.com>
2015-12-08 06:08:52 -0500

    point

--- pointLab/Point.java
+++ pointLab/Point.java
@@ -1,13 +1,28 @@
 public class Point{
 
-    private int _x;
-    private int _y;
+    // instance variables
+    private int _x, _y;
+
+    // default constructor:
+    // -no return type
+    // -same name as the calss
+    // -the parameter list is empty
+    // defalut point: (0,0)
+    public Point() {
+	_x = _y = 0;
+    }
 
     public Point(int x, int y) {
 	_x = x;
 	_y = y;
     }
 
+    public Point(Point other) {
+	_x = other.getX();
+	_y = other.getY();
+    }
+
+    // accessor methods: report back the state of an object (read only access)
     public int getX() {
 	return _x;
     }
@@ -16,14 +31,36 @@ public class Point{
 	return _y;
     }
 
-    //@override:
-    public String toString(Point p) {
+    //public Point getLocation() {
 	
+	//}
+
+    // @override
+    // post condition : "(x,y)"
+    public String toString(Point p) {
+	return "(" + _x + "," + _y + ")";
+    }
+
+    // @override
+    public boolean equals(Point p) {
+	if (_x == p.getX() && _y == p.getY()) return true;
+	return false;
     }
 
     public void move(int x, int y) {
 	_x += x;
 	_y += y;
+    }
+
+    public static void main(String[] args) {
+	Point a = new Point();
+	Point b = new Point();
+	
+	System.out.println(a);
+	System.out.println(b);
+	
+	System.out.println(a.getX());
+	System.out.println(a.getY());
     }
 
 }
