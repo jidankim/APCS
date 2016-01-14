9b0b22b6e3682bbcf35a6e247c1f3b25327a95ba
Dan Kim <jidankim@gmail.com>
2016-01-14 05:28:45 -0500

    data analysis

--- dataAnalysisLab/DataAnalysis.java
+++ dataAnalysisLab/DataAnalysis.java
@@ -8,7 +8,26 @@ public class DataAnalysis {
     public static int modeIndex(int[] data) {
 // precondition:  data is unimodal and data.length ≥ 3
 	int ans = -1;
-	for (int i = 0; i < data.length; i++) 
+	for (int i = 0; i < data.length - 1; i++) 
 	    if (isMode(data, i)) ans = i;
+	return ans;
+    }
+
+    public static void printHistogram(int[] data, int longestBar, String barChar) {
+// precondition:  data is unimodal and data.length ≥ 3;
+//                data[k] ≥ 0 for 0 ≤ k < data.length
+	for (int i = 0; i < data.length; i++) {
+	    int barLength = data[i] * longestBar / data[modeIndex(data)];
+	    System.out.print(barLength + " ");
+	    for (int j = 0; j < barLength; j++)
+		System.out.print(barChar);
+	    System.out.println();
+	}
+    }
+
+    public static void main(String[] args) {
+	int[] data = {3,5,9,10,12,11,9,4};
+	System.out.println("mode index: " + modeIndex(data));
+	printHistogram(data, 20, "X");
     }
 }
